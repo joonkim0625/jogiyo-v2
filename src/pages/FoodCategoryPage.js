a52bdd3e3f612a30f00f324643acd79627ce98df
Hyunjoon Kim <joonkim0625@gmail.com>
2019-01-09 01:12:21 +0900

    GPS 버튼 클릭 없이도 푸드카테고리 선택 가능하게 수정

--- src/pages/FoodCategoryPage.js
+++ src/pages/FoodCategoryPage.js
@@ -15,52 +15,46 @@ class FoodCategoryPage extends Component {
       <Layout>
         <div className="FoodCategory">
           <div className="FoodCategory__link-wrap">
-            {addrShow ? (
-              <>
-                <Link className="FoodCategory__link--all" to="/category">
-                  전체 보기
-                </Link>
-                <Link className="FoodCategory__link--alone" to="/category/8">
-                  1인분 주문
-                </Link>
-                <Link
-                  className="FoodCategory__link--franchise"
-                  to="/category/2"
-                >
-                  프랜차이즈
-                </Link>
-                <Link className="FoodCategory__link--chicken" to="/category/4">
-                  치킨
-                </Link>
-                <Link className="FoodCategory__link--pizza" to="/category/1">
-                  피자/양식
-                </Link>
-                <Link className="FoodCategory__link--chinese" to="/category/7">
-                  중식
-                </Link>
-                <Link className="FoodCategory__link--korean" to="/category/10">
-                  한식
-                </Link>
-                <Link
-                  className="FoodCategory__link--japanese"
-                  to="/category/12"
-                >
-                  일식/돈까스
-                </Link>
-                <Link className="FoodCategory__link--pig" to="/category/3">
-                  족발/보쌈
-                </Link>
-                <Link className="FoodCategory__link--night" to="/category/9">
-                  야식
-                </Link>
-                <Link className="FoodCategory__link--snack" to="/category/11">
-                  분식
-                </Link>
-                <Link className="FoodCategory__link--cafe" to="/category/5">
-                  카페/디저트
-                </Link>
-              </>
-            ) : (
+            {/* {click ? (
+              <> */}
+            <Link className="FoodCategory__link--all" to="/category">
+              전체 보기
+            </Link>
+            <Link className="FoodCategory__link--alone" to="/category/8">
+              1인분 주문
+            </Link>
+            <Link className="FoodCategory__link--franchise" to="/category/2">
+              프랜차이즈
+            </Link>
+            <Link className="FoodCategory__link--chicken" to="/category/4">
+              치킨
+            </Link>
+            <Link className="FoodCategory__link--pizza" to="/category/1">
+              피자/양식
+            </Link>
+            <Link className="FoodCategory__link--chinese" to="/category/7">
+              중식
+            </Link>
+            <Link className="FoodCategory__link--korean" to="/category/10">
+              한식
+            </Link>
+            <Link className="FoodCategory__link--japanese" to="/category/12">
+              일식/돈까스
+            </Link>
+            <Link className="FoodCategory__link--pig" to="/category/3">
+              족발/보쌈
+            </Link>
+            <Link className="FoodCategory__link--night" to="/category/9">
+              야식
+            </Link>
+            <Link className="FoodCategory__link--snack" to="/category/11">
+              분식
+            </Link>
+            <Link className="FoodCategory__link--cafe" to="/category/5">
+              카페/디저트
+            </Link>
+            {/* </> */}
+            {/* ) : (
               <>
                 <span
                   className="FoodCategory__link--all"
@@ -147,46 +141,9 @@ class FoodCategoryPage extends Component {
                   카페/디저트
                 </span>
               </>
-            )}
-            {/* <Link className="FoodCategory__link--all" to="/category">
-              전체 보기
-            </Link>
-            <Link className="FoodCategory__link--alone" to="/category/3">
-              1인분 주문
-            </Link>
-            <Link className="FoodCategory__link--franchise" to="/category/1">
-              프랜차이즈
-            </Link>
-            <Link className="FoodCategory__link--chicken" to="/category/2">
-              치킨
-            </Link>
-            <Link className="FoodCategory__link--pizza" to="/category/10">
-              피자/양식
-            </Link>
-            <Link className="FoodCategory__link--chinese" to="/category/4">
-              중식
-            </Link>
-            <Link className="FoodCategory__link--korean" to="/category/7">
-              한식
-            </Link>
-            <Link className="FoodCategory__link--japanese" to="/category/6">
-              일식/돈까스
-            </Link>
-            <Link className="FoodCategory__link--pig" to="/category/9">
-              족발/보쌈
-            </Link>
-            <Link className="FoodCategory__link--night" to="/category/5">
-              야식
-            </Link>
-            <Link className="FoodCategory__link--snack" to="/category/8">
-              분식
-            </Link>
-            <Link className="FoodCategory__link--cafe" to="/category/11">
-              카페/디저트
-            </Link> */}
+            )} */}
           </div>
         </div>
-        {/* <StoreList key={categories} categories={categories} /> */}
       </Layout>
     );
   }
